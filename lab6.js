dd597609c1e70fa9c4da08c8434d2d86d1cdb51e
Rhon Bramwell <xphat@xphat.net>
2018-11-12 07:19:02 +0000

    Excercise 3 - Completed!

--- lab6.js
+++ lab6.js
@@ -1,24 +1,77 @@
 window.onload = function()
 {
     var searchbutton = document.querySelector("#getdef");
+    var getallbutton = document.querySelector("#getalldefs");
+    
     
     searchbutton.addEventListener("click", function()
     {
+        clearresult();
         var srchtxt = document.querySelector("#txtbx").value;
-        getDef(srchtxt);
+        getDef(srchtxt,"http");
     }
     ,false);
     
-    function getDef(q)
+    
+    getallbutton.addEventListener("click", function()
+    {
+        clearresult();
+        var srchtxt = "&all=true";
+        getDef(srchtxt,"xml");
+    }
+    ,false);
+    
+    
+    function getDef(q,type)
     {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("result").innerHTML = this.responseText;
+            if (type == "http")
+            {
+                document.getElementById("result").innerHTML = this.responseText;
+            }
+            if (type == "xml")
+            {
+                var response = this.responseXML;
+                var result = document.querySelector('#result');
+                var entry = response.getElementsByTagName('definition');
+                
+                
+                var list = document.createElement('ol');
+                
+                
+                result.appendChild(list);
+                for (var x = 0; x < entry.length; x++) {
+                    
+                    var heading = document.createTextNode(entry[x].attributes[0].textContent);
+                    var authortext = "- " + entry[x].attributes[1].textContent;
+                    var author = document.createTextNode(authortext);
+                    var text = document.createTextNode(entry[x].textContent);
+                    var definition = document.createElement('li');
+                    
+                    var heading3 = document.createElement('h3');
+                    heading3.appendChild(heading);
+                    definition.appendChild(heading3);
+                    var npara = document.createElement('p');
+                    npara.appendChild(text);
+                    definition.appendChild(npara);
+                    var authpara = document.createElement('p');
+                    authpara.appendChild(author);
+                    definition.appendChild(authpara);
+                    list.appendChild(definition);
+                    
+                }
+            }
         }
       };
       var querystring = "request.php?q=" + q;
+      //console.log(querystring);
       xhttp.open("GET", querystring , true);
       xhttp.send();
+    }
+    
+    function clearresult() {
+        document.getElementById("result").innerHTML = "";
     }
 }
--- request.php
+++ request.php
@@ -4,6 +4,8 @@
 // respond with a value
 
 $query = $_GET['q'];
+$all = isset($_GET['all']) ? $_GET['all'] : false;
+
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -14,5 +16,23 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+if ($all == false)
+{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
+else
+{
+    $xmldata = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xmldata.= '<entries>';
+    foreach($definition as $entry => $definition)
+    {
+        $xmldata.= '<definition name = "' . strtoupper($entry) . '" author = "Author">';
+        $xmldata.= $definition;
+        $xmldata.= '</definition>';
+    }
+    $xmldata.= '</entries>';
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+}
